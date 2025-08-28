ab5c0408c07144b1406deea91d3347b3c5de599e
Magic-80 <developper.magic@gmail.com>
2025-08-28 13:33:50 +0200

    fix: - suppresion fichier inutile

--- client/kouak/src/store/auth.jsx
+++
@@ -1,8 +0,0 @@
-// import { create } from "zustand";
-
-// export const useAuth = create((set) => ({
-//   token: null,
-//   user: null,
-//   setAuth: (token, user) => set({ token, user }),
-//   logout: () => set({ token: null, user: null }),
-// }));
