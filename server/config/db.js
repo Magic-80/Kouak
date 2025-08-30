9828e7289d00ba8b135f42b022f730dc64d1df68
Magic-80 <developper.magic@gmail.com>
2025-08-30 18:14:52 +0200

    Ignore config/config.json et config/db.js

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("kouak", "root", "123abc", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
-
-module.exports = sequelize;
